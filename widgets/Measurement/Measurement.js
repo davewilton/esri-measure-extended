b1ddd28921fb63af4965a817a0be14116de69269
David Wilton <dave_wilton@msn.com>
2016-08-31 03:03:23 +0100

    initial add

---
+++ widgets/Measurement/Measurement.js
@@ -0,0 +1,144 @@
+define([
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "dojo/dom-construct",
+  "dojo/number",
+
+  "esri/dijit/Measurement",
+  "esri/geometry/Polyline",
+  "esri/geometry/geometryEngine",
+], function (
+  declare,
+  lang,
+  domConstruct,
+  Number,
+  esriMeasurement,
+  Polyline,
+  GeometryEngine,
+  Units
+) {
+    return declare([esriMeasurement], {
+      // description:
+      //  extends esri's measure tool to add last segment and current segment properties
+
+      //store our array of points as the user clicks
+      arrMeasurePoints: [],
+      //set this to false if you do not want to show the distance as the mouse moves. 
+      mouseMoveMeasure: true,
+      //div to display the results of the last segment measured
+      resultMouseSegmentDiv: null,
+      //div to display the current segment length as the mouse moves
+
+
+      postCreate: function () {
+
+        console.log('app.Measurement::postCreate', arguments);
+        this.inherited(arguments);
+
+        //hook into the map click event
+        this.map.on('click', lang.hitch(this, this.calcDistance));
+
+        //hook in to clear the points on draw end
+        this.own(this.on("tool-change", lang.hitch(this, function () {
+          this.arrMeasurePoints = [];
+          this.resultSegmentDiv.innerHTML = "&nbsp;"
+          if (this.resultMouseSegmentDiv) this.resultMouseSegmentDiv.innerHTML = "&nbsp;";
+        })));
+        this.own(this.on("measure-end", lang.hitch(this, function () {
+          this.arrMeasurePoints = [];
+        })));
+
+        if (this.mouseMoveMeasure) {
+          this.own(this.map.on('mouse-move', lang.hitch(this, this.calcMouseDistance)));
+        }
+
+        //add a new div to contain our mouse segment length
+        if (this.mouseMoveMeasure) this.resultMouseSegmentDiv = domConstruct.create('div', { 'class': 'esriMeasurementResultSegment', innerHTML: '&nbsp;' }, this.resultValueContainer.domNode);
+        //add a new div to contain our segment length
+        this.resultSegmentDiv = domConstruct.create('div', { 'class': 'esriMeasurementResultSegment', innerHTML: '&nbsp;' }, this.resultValueContainer.domNode)
+
+      },
+
+      calcDistance: function (evt) {
+        try {
+
+          //we are only going to do this for distance
+          if (!this.activeTool || this.activeTool.toString().toLowerCase() !== 'distance') return false;
+
+          this.arrMeasurePoints.push(evt.mapPoint);
+
+          //if we have more than two create a line and measure them
+          if (this.arrMeasurePoints.length < 2) return false;
+
+          var length = this._createAndMeasureLine(this.arrMeasurePoints[this.arrMeasurePoints.length - 2], this.arrMeasurePoints[this.arrMeasurePoints.length - 1]);
+
+          this._displaySegmentResult(length);
+
+          return true;
+
+        } catch (e) {
+          return false;
+        }
+      },
+
+      calcMouseDistance: function (evt) {
+        //if we have a point to work with we can continue
+        try {
+
+          //we are only going to do this for distance
+          if (!this.activeTool || this.activeTool.toString().toLowerCase() !== 'distance') return false;
+
+          if (this.arrMeasurePoints.length < 1) return false;
+
+          var length = this._createAndMeasureLine(this.arrMeasurePoints[this.arrMeasurePoints.length - 1], evt.mapPoint);
+
+          this._displayMouseSegmentResult(length);
+
+          return true;
+
+        } catch (e) {
+          return false;
+        }
+      },
+
+      _displaySegmentResult: function (length) {
+        //format the number.
+        length = this._formatNumberForDisplay(length);
+        this.resultSegmentDiv.innerHTML = "Last Segment lengh: " + length + " " + this.getUnit();
+      },
+
+      _displayMouseSegmentResult: function (length) {
+        //format the number.
+        length = this._formatNumberForDisplay(length);
+        this.resultMouseSegmentDiv.innerHTML = "Segment lengh: " + length + " " + this.getUnit();
+      },
+
+      _formatNumberForDisplay: function (num) {
+        return Number.format(num, { pattern: this.numberPattern + "0" });
+      },
+
+      _createAndMeasureLine: function(point1, point2) {
+
+        var line = new Polyline(this.map.spatialReference);
+        //use the last two points 
+        line.addPath([point1, point2]);
+
+        //now use the geometry engine to measure it's lengh
+        //is it WGS-84 or Web Mercator Auxiliary Sphere? If so use geodesic
+        //geodesicLength(geometry, unit)
+        var unit = this.getUnit();
+        if (!unit) unit = 'kilometers';
+        unit = unit.toLowerCase().replace(' ', '-');
+        var length;
+        if (this.map.spatialReference.isWebMercator()) {
+          length = GeometryEngine.geodesicLength(line, unit);
+        } else {
+          length = GeometryEngine.planarLength(line, unit);
+        }
+
+        return length;
+      }
+
+
+    });
+  });
---
+++ widgets/Measurement/test/spec/MeasurementSpec.js
@@ -0,0 +1,122 @@
+define([
+  'app/widgets/Measurement/Measurement',
+  'esri/map',
+  'dojo/dom-construct',
+  'dojo/_base/lang'
+
+], function (
+  WidgetUnderTest,
+  Map,
+  domConstruct,
+  lang
+) {
+    describe('app/widgets/Measurement', function () {
+      var widget;
+      var destroy = function (widget) {
+        if (widget && widget.destroyRecursive) {
+          widget.destroyRecursive();
+          widget = null;
+        }
+      };
+
+      beforeEach(function (done) {
+        // init map. I don't really want to use a map in tests but as the esri measure widget requires it I don't think I can get away from it
+        var mapOptions = {
+          basemap: 'topo',
+          center: [-122.45, 37.75],
+          zoom: 13,
+          sliderStyle: 'small'
+        };
+
+        var map = new Map(domConstruct.create('div', null, document.body), mapOptions);
+        map.on('load', lang.hitch(this, function (m) {
+          // init widget
+          widget = new WidgetUnderTest({ map: map }, domConstruct.create('div', null, document.body));
+          done();
+        }));
+
+      });
+
+      afterEach(function () {
+        destroy(widget);
+      });
+
+      describe('Sanity', function () {
+        it('should create a Measurement', function () {
+          expect(widget).to.not.be.an("undefined");
+        });
+      });
+
+
+      describe('measure lines', function () {
+
+        it('should add two divs for display of results', function () {
+          expect(widget.resultSegmentDiv).to.not.be.an("null");
+          expect(widget.resultMouseSegmentDiv).to.not.be.an("null");
+        });
+
+        it('two points should be measured', function () {
+          var length = widget._createAndMeasureLine([55, 55], [60, 60])
+          assert.equal(length, 0.007047439347675955);
+        });
+
+        it('format numbers for display', function () {
+          var disp = widget._formatNumberForDisplay(10)
+          assert.equal("10.00", disp);
+        });
+
+         it('should only work on distance', function () {
+          widget.activeTool = "test";
+          var val = widget.calcDistance({});
+          assert.equal(val, false);
+          val = widget.calcMouseDistance({});
+          assert.equal(val, false);
+        });
+
+         it('Map point should be stored when in distance mode but it should not calculate length (it needs two points)', function () {
+          widget.activeTool = "distance";
+          var val = widget.calcDistance({mapPoint: {}});
+          assert.equal(val, false);
+        });
+
+        it('Mouse distance only requires one point in the array and the passed in point', function () {
+          widget.activeTool = "distance";
+          var val = widget.calcMouseDistance({mapPoint: {}});
+          assert.equal(val, true);
+        });
+
+        it('Mouse distance should not store that point in the array', function () {
+          widget.activeTool = "distance";
+          var countBefore = widget.arrMeasurePoints.length;
+          var val = widget.calcMouseDistance({mapPoint: {}});
+          assert.equal(val, true);
+          assert.equal(countBefore, widget.arrMeasurePoints.length);
+        });
+
+        it('Map point should be stored when in distance mode and it will calculate length', function () {
+          widget.activeTool = "distance";
+          var val = widget.calcDistance({mapPoint: {}});
+          assert.equal(val, true);
+        });
+
+        it('Check two map points are in the array', function () {
+          assert.equal(widget.arrMeasurePoints.length, 2);
+        });
+
+        it('Check that the result is displayed for last segment', function () {
+          widget._unitDropDown.label = "kilometers";
+          widget._displaySegmentResult(10);
+          assert.equal(widget.resultSegmentDiv.innerHTML, "Last Segment lengh: 10.00 kilometers"); 
+        });
+
+        it('Check that the result is displayed for last mouse segment', function () {
+          widget._unitDropDown.label = "kilometers";
+          widget._displayMouseSegmentResult(10);
+          assert.equal(widget.resultMouseSegmentDiv.innerHTML, "Segment lengh: 10.00 kilometers"); 
+        });
+
+
+      });
+
+    });
+  });
